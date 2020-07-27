d27c8384cba786341f0d6807542cfeff5c63edee
4Butterfly <65900362+4Butterfly@users.noreply.github.com>
2020-07-27 18:53:57 +0500

    add something new
    testing

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(function () {
+
+	// Custom JS
+	particlesJS.load('particles-js', '../libs/particlesjs/particles.json', function() {
+		console.log('callback - particles.js config loaded');
+	});
+});
