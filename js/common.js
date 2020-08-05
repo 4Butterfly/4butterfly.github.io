7e9a3bd3276471e273e27bdd6548bd37c06277e0
4Butterfly <65900362+4Butterfly@users.noreply.github.com>
2020-08-05 18:00:48 +0300

    another "important" update

--- js/common.js
+++ js/common.js
@@ -1,7 +1,30 @@
 $(function () {
 
 	// Custom JS
-	particlesJS.load('particles-js', '../libs/particlesjs/particles.json', function() {
+	particlesJS.load('particles-js', '../libs/particlesjs/particles.json', function () {
 		console.log('callback - particles.js config loaded');
+	});
+
+	$(".owl-carousel").owlCarousel({
+		loop: 'true',
+		autoplay: 'true',
+		autoplaySpeed:'2000',
+		autoplayTimeout:'2000',
+		autoplayHoverPause: 'true',
+		center: 'true',
+		responsive:{
+			0:{
+					items:1
+			},
+			425:{
+					items:2
+			},
+		765:{
+					items:3
+			},
+			1023:{
+				items:4
+		}
+	}
 	});
 });
